c5095501ff79d94f0b5b4ac7948e798bec9275c6
shovalsolo <shovalso@gmail.com>
2020-02-14 18:29:56 -0500

    Adding an Template file

---
+++ CypressAutomation/cypress/integration/e3eTest/template.spec.js
@@ -0,0 +1,13 @@
+//This is a template file 
+
+describe('Template demo', function(){
+
+	it('Login to page', function(){
+		
+		cy.visit('https://www.saucedemo.com/index.html')					//Will open the link from cypress.json baseUrl
+		cy.get('').type('')													//Will search an id and will send keys
+		cy.get('').type('')													//Will search an id and will send keys
+		cy.get('').click()													//Will search an id and will click on it
+		
+	})
+})
