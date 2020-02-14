cce59b63f217c6a04b8e262cc00847f84477c253
shovalsolo <shovalso@gmail.com>
2020-02-14 15:46:05 -0500

    Updating todo

---
+++ CypressAutomation/cypress-tutorial-build-todo-starter-master/src/lib/service.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export const saveTodo = (todo) =>
+	axios.post('http://localhost:3030/api/todos',todo)
+	
--- CypressAutomation/cypress/integration/examples/input-form.spec.js
+++ CypressAutomation/cypress/integration/examples/input-form.spec.js
@@ -1,5 +1,35 @@
-describe('Input form', ()=>{
-	it('focuses input on load',()=>{
-		cy.visit('http://localhost:3030/')
+describe('Input form', () => {
+	
+	beforeEach(() => {								//This will run before each test
+		cy.visit('/')								//tacking the url from cypress.json file
 	})
+	
+	
+	it('focuses input on load',() => {				//This will check that we have this class 
+		
+		cy.focused().should('have.class','new-todo')
+	})
+	
+	
+	it('accepts input failed',() => {				//fail the check theat the text field exist when adding .only it will run only this test
+	
+		cy.get('.new-toda')
+	})
+	
+	it('accepts input',() => {						//Will check theat the text field exist
+		const typedText = 'Buy Milk'
+		
+		cy.get('.new-todo')
+			.type(typedText)
+			.should('have.value', typedText)
+	})
+	
+	context('Form submission', () => {				//
+		it.only('Adds a new todo on submit', () => {//
+			cy.get('.new-todo')						//
+				.type('Buy eggs')					//Send text
+				.type('{enter}')					//Press on enter
+		})
+	})
+	
 })
