876b3064e5c5963c41fe697d5d8ef82f90415431
shovalsolo <shovalso@gmail.com>
2020-02-28 15:40:54 -0500

    Updating comments

--- CypressAutomation/cypress/integration/e3eTest/jsAlert.spec.js
+++ CypressAutomation/cypress/integration/e3eTest/jsAlert.spec.js
@@ -10,7 +10,7 @@ describe('Handeling alerts demo', function(){
 		cy.contains('Click for JS Alert').click()			//Clicking on the button to trigger the alert
 	})
 	
-	it.only('Java script alert get text', function(){
+	it('Java script alert get text', function(){
 		cy.on('window:alert', function(alertText){			//Getting the window of the alert
 			expect(alertText).eq('I am a JS Alert')			//Checking the thext of the alert
 		})
